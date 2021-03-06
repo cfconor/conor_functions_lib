edbd52e40826a947bd33471e0df28ffbfcf1208b
Conor Creagh-Peschau <cfconor@gmail.com>
2021-03-06 18:29:15 +0000

    trying to update the stringify method to work correctly

--- python/data_manipulation.py
+++ python/data_manipulation.py
@@ -2,12 +2,12 @@ def stringify(*argv):
     """
     Takes one or more variables as arguments and returns a string containing the arguments
     """
-    return_string = ""
+    return_string = []
 
     for arg in argv:
         arg_string = str(arg)
-        return_string.join(arg_string)
+        return_string.append(arg_string)
 
-    return return_string
+    return str(return_string)
 
 print(stringify("hello ","world"))
